1f344818bbd3c7a08d1a40fa71314b1f31dd193c
Seth Coleman <colemans@moravian.edu>
2023-11-19 22:30:27 -0500

    fix repo cloning in / instead of ~

--- aws-setup-git-script.sh
+++ aws-setup-git-script.sh
@@ -3,8 +3,7 @@
 # This is so that I have to copy paste less when I'm testing
 
 sudo yum install -y git
-git clone https://github.com/cs298-398f23/magic_randomizer.git
-cd magic_randomizer
+git clone https://github.com/cs298-398f23/magic_randomizer.git /home/ec2-user/magic_randomizer
 
-chmod +x ./magic_randomizer/start-script.sh
-source magic_randomizer/start-script.sh
+chmod +x /home/ec2-user/magic_randomizer/start-script.sh
+source /home/ec2-user/magic_randomizer/start-script.sh
