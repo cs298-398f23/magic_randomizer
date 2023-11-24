167077f94bb34673b983b0e8d534c1c79f825078
Seth Coleman <colemans@moravian.edu>
2023-11-23 19:34:33 -0500

    added testing webpage and made flask server work locally

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    @app.route("/")
+    def hello_world():
+        return open("index.html").read()
+    
+    return app
+
+def launch_app():
+    app = create_app()
+    app.run(port=8000, host="0.0.0.0")
+
+if __name__ == "__main__":
+    launch_app()
