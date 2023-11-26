0b5fb660cc2ee55377133f6540c728dccbee18cf
Seth Coleman <colemans@moravian.edu>
2023-11-26 17:32:26 -0500

    added a save and load deck feature to the demo

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask import render_template
+from pymongo.mongo_client import MongoClient
+from flask import request
+from bson import json_util
+import json
 
 def create_app():
     app = Flask(__name__)
@@ -7,6 +11,47 @@ def create_app():
     @app.route("/")
     def hello_world():
         return render_template("index.html")
+    
+    @app.route("/save", methods=["POST"])
+    def save():
+        mongoClient = MongoClient("mongodb://localhost:27017")
+
+        cardList = request.data.decode("utf-8")
+        cardList = cardList.split("\n")
+        
+        json_card_list = process_card_list(cardList)
+
+        cardList = {"cards": json_card_list}
+        
+        try:
+            mongoClient.magic_randomizer.decks.insert_one(cardList) # collection.deeper_collection.insert_one(document)
+            return "Deck Added successfully!", 200
+        except Exception as e:
+            result = e
+            return "", 500
+        
+    @app.route("/load", methods=["GET"])
+    def load():
+        mongoClient = MongoClient("mongodb://localhost:27017")
+        decks = mongoClient.magic_randomizer.decks.find()
+        decks = list(decks)
+        
+        data = {"decks": decks}
+        data = json.loads(json_util.dumps(data))
+        return data, 200
+
+    def process_card_list(cardList):
+        """
+        Processes the card list and returns a json format of cards
+        
+        """
+        # TODO: Add error handling for invalid card list
+        json_card_list = []
+        for card in cardList:
+            card = card.split(" ")
+            card = {"count": card[0], "name": " ".join(card[1:])} # joins the name in case the card name has spaces
+            json_card_list.append(card)
+        return json_card_list
     
     return app
 
--- static/app.js
+++ static/app.js
@@ -9,3 +9,27 @@ document.getElementById("card_entry_button").addEventListener("click", function 
             document.getElementById("card_image").src = json.data[0].image_uris.normal;
         });
 });
+
+document.getElementById("deck_entry_button").addEventListener("click", function () {
+    let body = document.getElementById("deck_entry_box").value;
+    fetch("/save", {body, method: "POST"})
+    .then(function (response) {
+        return response;
+    }).then(function (response) {
+        if (response.status === 200) {
+            document.getElementById("deck_entry_button").style.backgroundColor = "green";
+        } else {
+            console.log(response);
+            document.getElementById("deck_entry_button").style.backgroundColor = "red";
+        }
+    });
+});
+
+document.getElementById("deck_load_button").addEventListener("click", function () {
+    fetch("/load")
+    .then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        document.getElementById("decks").textContent = JSON.stringify(json);
+    });
+});
