c726b829983ece1c64fce2d414fdcf77cac5ea0a
Carl R <carl@laptop-2.local>
2023-11-26 16:28:29 -0500

    Dockerfile and hello.py

--- hello.py
+++ hello.py
@@ -1,18 +1,11 @@
 from flask import Flask
 from flask import render_template
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route("/")
-    def hello_world():
-        return render_template("index.html")
-    
-    return app
-
-def launch_app():
-    app = create_app()
-    app.run(port=8000, host="0.0.0.0", debug=True)
+@app.route("/")
+def hello():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    launch_app()
+    app.run(port=8000, host="0.0.0.0", debug=True)
